9a10612522828e75da0659e3c72721e00e639e9f
AndryKuz <fmeon1992@gmail.com>
2023-12-04 17:11:00 +0200

    finish

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -182,7 +182,7 @@ const Header = () => {
                 <path d="M158.08,165.49a15,15,0,0,1-14.23-10.26L118.14,78H70.7a15,15,0,1,1,0-30H129a15,15,0,0,1,14.23,10.26l29.13,87.49a15,15,0,0,1-14.23,19.74Z" />
               </g>
             </svg>
-            {!!cart.length && <span className={cl.count}>{cart.length}</span>}
+            {!!cart.length && (<span className={cl.count}>{cart.length}</span>)}
             
           </Link>
         </div>
